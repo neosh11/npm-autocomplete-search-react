a9405f87735fc7bb1964bca605d6817e4cccc4a9
Neosh <neosh11@gmail.com>
2023-03-23 18:53:22 +1100

    Add tokenizer support

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { useEffect, useMemo, useState } from 'react';
 import { String2ObjectAutoCompleteSearch } from '@neosh11/autocomplete-search';
 
-export function useSearch(data: any[], searchKey: string | ((arg0: any) => string), maxResults: number) {
+export function useSearch(
+  data: any[],
+  searchKey: string | ((arg0: any) => string),
+  maxResults: number,
+  tokenizer?: RegExp | string,
+) {
   const [searchText, setSearchText] = useState('');
   const [filteredObjects, setFilteredObjects] = useState(data);
   const autoCompleteSearch = useMemo(() => {
     const searchOptions = {
       ignoreCase: true,
       objectIdProperty: 'id',
-      tokenizer: ' ',
+      tokenizer: tokenizer || ' ',
     };
     return new String2ObjectAutoCompleteSearch(searchOptions);
-  }, []);
+  }, [tokenizer]);
   useEffect(() => {
     autoCompleteSearch.clear();
     // fill autoCompleteSearch
